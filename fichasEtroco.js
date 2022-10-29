1109195e0f9a99518c33663f56d03ab4ef8f21fd
matheus <muilokomota@gmail.com>
2022-10-29 09:23:34 -0300

    auteracoes em cadastro de usuarios e fichas e troco, e criacao do arquivo html.

--- fichasEtroco.js
+++ fichasEtroco.js
@@ -1,5 +1,5 @@
 function compraFichas(dinheiroRecebido) {
-    quantidadeFichas = parseInt(dinheiroRecebido / 5)
-    troco = dinheiroRecebido - quantidadeFichas * 5 
-    return [quantidadeFichas, troco]
-} 
+    quantidadeFichas = parseInt(dinheiroRecebido / 5);
+    troco = dinheiroRecebido - quantidadeFichas * 5;
+    return [quantidadeFichas, troco];
+}
